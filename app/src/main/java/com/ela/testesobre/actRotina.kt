ddace3b00f986dac022fec6d040259c007502181
Ale TI <alessandrodev@outlook.com>
2023-07-17 12:41:58 -0300

    commit interfaces

---
+++ app/src/main/java/com/ela/testesobre/actRotina.kt
@@ -0,0 +1,11 @@
+package com.ela.testesobre
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class actRotina : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_act_rotina)
+    }
+}
