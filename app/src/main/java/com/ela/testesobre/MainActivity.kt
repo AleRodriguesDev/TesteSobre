b3e05e5378f86bbb00415e56d03f663692f801c5
Ale TI <alessandrodev@outlook.com>
2023-07-17 14:20:38 -0300

    commit intent

--- app/src/main/java/com/ela/testesobre/MainActivity.kt
+++ app/src/main/java/com/ela/testesobre/MainActivity.kt
@@ -1,19 +1,29 @@
 package com.ela.testesobre
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import com.ela.testesobre.databinding.ActivityMainBinding
 
-lateinit var binding: ActivityMainBinding
+ private lateinit var binding: ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(LayoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.imgSobre.setOnClickListener(){
-            val intent = new
+            val intent = Intent(this, actSobre::class.java)
+            startActivity(intent)
+        }
+        binding.imgRotina.setOnClickListener(){
+            val intent = Intent(this, actRotina::class.java)
+            startActivity(intent)
+        }
+        binding.imgContatos.setOnClickListener(){
+            val intent = Intent(this, actContatos::class.java)
+            startActivity(intent)
         }
     }
 }
